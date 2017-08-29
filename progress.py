22dfcfb11f9c9c0f07b9f99bf55d7f0295bc6516
NateScarlet <NateScarlet@Gmail.com>
2017-08-29 13:52:29 +0800

    progress 0.3.0 # Add Qt version progressbar

--- progress.py
+++ progress.py
@@ -1,12 +1,49 @@
 # -*- coding=UTF-8 -*-
 """Show progress to user.  """
-try:
-    import nuke
-    HAS_NUKE = True
-except ImportError:
-    HAS_NUKE = False
+import os
+import sys
 
-__version__ = '0.1.1'
+from .Qt import QtCompat, QtWidgets
+from .files import HAS_NUKE
+
+if HAS_NUKE:
+    import nuke
+
+__version__ = '0.3.0'
+
+
+class ProgressBar(QtWidgets.QDialog):
+    """Qt progressbar dialog."""
+
+    def __init__(self):
+        self._cancelled = False
+
+        app = QtWidgets.QApplication.instance()
+        if not app:
+            app = QtWidgets.QApplication(sys.argv)
+        super(ProgressBar, self).__init__()
+        QtCompat.loadUi(os.path.join(__file__, '../progress.ui'), self)
+        self.show()
+
+    def setProgress(self, value):
+        """Set progress value.  """
+        self.progressBar.setValue(value)
+
+    def setMessage(self, message):
+        """Set progress message.  """
+        self.setWindowTitle(message)
+
+    def isCancelled(self):
+        """Return if cancel button been pressed.  """
+        return self._cancelled
+
+    def reject(self):
+        """Override QDiloag.reject()"""
+        self._cancelled = True
+
+    def closeEvent(self, event):
+        """Override QWidget.closeEvent()"""
+        event.ignore()
 
 
 class Progress(object):
@@ -16,28 +53,27 @@ class Progress(object):
     def __init__(self, name=''):
         if HAS_NUKE:
             self._task = nuke.ProgressTask(name)
+        else:
+            self._task = ProgressBar()
+            self._task.setMessage(name)
+
+    def __del__(self):
+        if not HAS_NUKE:
+            self._task.hide()
+        del self._task
 
     def set(self, progress=None, message=None):
         """Set progress number and message"""
 
-        if self.is_cancelled():
+        if self._task.isCancelled():
             raise CancelledError
 
-        if HAS_NUKE:
-            if progress:
-                self._task.setProgress(progress)
-            if message:
-                self._task.setMessage(message)
-        else:
-            print('{}% {}'.format(progress, message if message else ''))
-
-    def is_cancelled(self):
-        """Return if cancel button been pressed.  """
-
-        if HAS_NUKE:
-            return self._task.isCancelled()
-
-        return False
+        if progress:
+            self._task.setProgress(progress)
+        if message:
+            self._task.setMessage(message)
+        if not HAS_NUKE:
+            QtWidgets.QApplication.processEvents()
 
 
 class CancelledError(Exception):
