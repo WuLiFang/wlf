5ee51eacbc97380828047bea730ca0d9bed51867
NateScarlet <NateScarlet@Gmail.com>
2017-09-08 17:00:44 +0800

    message 0.1.0

---
+++ message.py
@@ -0,0 +1,35 @@
+# -*- coding=UTF-8 -*-
+import sys
+import multiprocessing
+import threading
+
+from .Qt import QtWidgets
+
+__version__ = '0.1.0'
+
+
+def _error_process(message, error_type=''):
+    app = QtWidgets.QApplication(sys.argv)
+    dummy_var = _error_message(message, error_type)
+    sys.exit(app.exec_())
+
+
+def _error_message(message, error_type=''):
+    frame = QtWidgets.QErrorMessage()
+    frame.showMessage(message, error_type)
+    frame.show()
+    return frame
+
+
+def error(message, error_type=''):
+    """Show error message. """
+
+    def _run():
+        proc = multiprocessing.Process(
+            target=_error_process, args=(message, error_type))
+        proc.start()
+        proc.join()
+    if not QtWidgets.QApplication.instance():
+        threading.Thread(target=_run, name='ErrorDialog').start()
+    else:
+        _error_message(message, error_type)
