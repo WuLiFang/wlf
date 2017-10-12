9be6c21f5547d768ffbf78248612b63111ec89be
NateScarlet <NateScarlet@Gmail.com>
2017-10-12 14:49:25 +0800

    console 0.1.0

--- console.py
+++ console.py
@@ -3,14 +3,21 @@
 
 import sys
 import time
+from subprocess import call
+
+__version__ = '0.1.0'
 
 
-def pause():
+def pause(timeout=5):
     """Pause prompt with a countdown."""
 
-    print(u'')
-    for i in range(5)[::-1]:
-        sys.stdout.write(u'\r{:2d}'.format(i + 1))
-        time.sleep(1)
-    sys.stdout.write(u'\r          ')
-    print(u'')
+    if timeout <= 0:
+        if sys.platform == 'win32':
+            call('PAUSE', shell=True)
+    else:
+        print(u'')
+        for i in range(timeout)[::-1]:
+            sys.stdout.write(u'\r{:2d}'.format(i + 1))
+            time.sleep(1)
+        sys.stdout.write(u'\r          ')
+        print(u'')
