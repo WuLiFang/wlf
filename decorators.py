9f9abba7955a5b8fc52e16233e4869dd1dbdc199
NateScarlet <NateScarlet@Gmail.com>
2017-11-02 15:39:34 +0800

    decorators 0.1.0 # Add run_with_clock

--- decorators.py
+++ decorators.py
@@ -1,7 +1,18 @@
 """Function decorators.  """
 
+from __future__ import print_function, unicode_literals
+
+import logging
+import time
+import inspect
+
 from functools import wraps
 import threading
+
+LOGGER = logging.getLogger('com.wlf.decorators')
+assert isinstance(LOGGER, logging.Logger)
+
+__version__ = '0.1.0'
 
 
 def run_async(func):
@@ -12,4 +23,23 @@ def run_async(func):
         thread = threading.Thread(target=func, args=args, kwargs=kwargs)
         thread.start()
         return thread
+    return _func
+
+
+def run_with_clock(func):
+    """Run func with a clock.  """
+
+    callerframerecord = inspect.stack()[1]
+    frame = callerframerecord[0]
+    info = inspect.getframeinfo(frame)
+    func_desc = '{0.filename}:{0.lineno}:{1}'.format(info, func.__name__)
+
+    @wraps(func)
+    def _func(*args, **kwargs):
+        start_time = time.clock()
+        try:
+            return func(*args, **kwargs)
+        finally:
+            LOGGER.info('%s:cost %.2f seconds',
+                        func_desc, time.clock() - start_time)
     return _func
