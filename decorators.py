7161d64000f92c78d701c241d992bcca51950bdf
NateScarlet <NateScarlet@Gmail.com>
2017-11-02 15:56:23 +0800

    decorator 0.1.1 # Add custom name support on run_with_clock

--- decorators.py
+++ decorators.py
@@ -1,3 +1,4 @@
+# -*- coding=UTF-8 -*-
 """Function decorators.  """
 
 from __future__ import print_function, unicode_literals
@@ -12,7 +13,7 @@ import threading
 LOGGER = logging.getLogger('com.wlf.decorators')
 assert isinstance(LOGGER, logging.Logger)
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 def run_async(func):
@@ -26,20 +27,28 @@ def run_async(func):
     return _func
 
 
-def run_with_clock(func):
+def run_with_clock(name=None):
     """Run func with a clock.  """
 
-    callerframerecord = inspect.stack()[1]
-    frame = callerframerecord[0]
-    info = inspect.getframeinfo(frame)
-    func_desc = '{0.filename}:{0.lineno}:{1}'.format(info, func.__name__)
+    assert isinstance(name, (str, unicode)),\
+        'Expected str type, got {}'.format(type(name))
 
-    @wraps(func)
-    def _func(*args, **kwargs):
-        start_time = time.clock()
-        try:
-            return func(*args, **kwargs)
-        finally:
-            LOGGER.info('%s:cost %.2f seconds',
-                        func_desc, time.clock() - start_time)
-    return _func
+    def _wrap(func):
+        callerframerecord = inspect.stack()[1]
+        frame = callerframerecord[0]
+        info = inspect.getframeinfo(frame)
+        func_desc = '{0.filename}: {0.lineno}: {1}'.format(info, func.__name__)
+
+        @wraps(func)
+        def _func(*args, **kwargs):
+            start_time = time.clock()
+            try:
+                return func(*args, **kwargs)
+            finally:
+                cost_time = time.clock() - start_time
+                LOGGER.debug('%s: cost %.2f seconds',
+                             func_desc, cost_time)
+                if name is not None:
+                    LOGGER.info('%s 耗时 %.2f 秒', name, cost_time)
+        return _func
+    return _wrap
