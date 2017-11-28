2001a5c902b01b5fa6d5e5a7b094a8fd9ccc92a8
NateScarlet <NateScarlet@Gmail.com>
2017-11-28 17:39:36 +0800

    path 0.2.0 # Become object-orient

--- path.py
+++ path.py
@@ -3,16 +3,17 @@
 
 from __future__ import print_function, unicode_literals
 
+import os
 import re
 import json
 import locale
 import string
 import logging
-from wlf.pathlib2 import PurePath, Path
+import wlf.pathlib2 as pathlib
 
-__version__ = '0.1.6'
+__version__ = '0.2.0'
 
-with Path(Path(__file__) / '../files.tags.json').open() as _f:
+with pathlib.Path(pathlib.Path(__file__) / '../files.tags.json').open(encoding='UTF-8') as _f:
     _TAGS = json.load(_f)
     REGULAR_TAGS = _TAGS['regular_tags']
     TAG_CONVERT_DICT = _TAGS['tag_convert_dict']
@@ -21,84 +22,6 @@ with Path(Path(__file__) / '../files.tags.json').open() as _f:
 del _TAGS, _f
 
 LOGGER = logging.getLogger('com.wlf.path')
-
-
-def expand_frame(filename, frame):
-    '''Return a frame mark expaned version of filename, with given frame.
-
-    >>> expand_frame('test_sequence_###.exr', 1)
-    u'test_sequence_001.exr'
-    >>> expand_frame('test_sequence_369.exr', 1)
-    u'test_sequence_369.exr'
-    >>> expand_frame('test_sequence_%03d.exr', 1234)
-    u'test_sequence_1234.exr'
-    >>> expand_frame('test_sequence_%03d.###.exr', 1234)
-    u'test_sequence_1234.1234.exr'
-    '''
-    def _format_repl(matchobj):
-        return matchobj.group(0) % frame
-
-    def _hash_repl(matchobj):
-        return '%0{}d'.format(len(matchobj.group(0)))
-    ret = filename
-    ret = re.sub(r'(\#+)', _hash_repl, ret)
-    ret = re.sub(r'(%0?\d*d)', _format_repl, ret)
-    return ret
-
-
-def split_version(f):
-    """Return nuke style _v# (shot, version number) pair.
-
-    >>> split_version('sc_001_v20.nk')
-    (u'sc_001', 20)
-    >>> split_version('hello world')
-    (u'hello world', None)
-    >>> split_version('sc_001_v-1.nk')
-    (u'sc_001_v-1', None)
-    >>> split_version('sc001V1.jpg')
-    (u'sc001', 1)
-    >>> split_version('sc001V1_no_bg.jpg')
-    (u'sc001', 1)
-    >>> split_version('suv2005_v2_m.jpg')
-    (u'suv2005', 2)
-    """
-
-    match = re.match(r'(.+)v(\d+)', f, flags=re.I)
-    if not match:
-        return (unicode(PurePath(f).stem), None)
-    shot, version = match.groups()
-    return (shot.strip('_'), int(version))
-
-
-def remove_version(path):
-    """Return filename without version number.
-
-    >>> remove_version('sc_001_v233.jpg')
-    u'sc_001.jpg'
-    """
-    shot = split_version(path)[0]
-    ext = PurePath(path).suffix
-    return '{}{}'.format(shot, ext)
-
-
-def get_footage_name(path):
-    """Return filename without frame number.
-
-    >>> get_footage_name('sc_001_BG.0034.exr')
-    u'sc_001_BG'
-    >>> get_footage_name('sc_001_BG.%04d.exr')
-    u'sc_001_BG'
-    >>> get_footage_name('sc_001_BG.###.exr')
-    u'sc_001_BG'
-    >>> get_footage_name('sc_001._BG.exr')
-    u'sc_001._BG'
-    """
-    ret = path
-    ret = re.sub(r'\.\d+\b', '', ret)
-    ret = re.sub(r'\.#+(?=\.)', '', ret)
-    ret = re.sub(r'\.%0?\d*d\b', '', ret)
-    ret = unicode(PurePath(ret).stem)
-    return ret
 
 
 def get_unicode(input_str, codecs=('UTF-8', 'GBK')):
@@ -138,93 +61,6 @@ def is_ascii(text):
         return False
 
 
-def get_layer(filename, layers=None):
-    """Return layer name from @filename.
-
-    >>> get_layer('Z:/MT/Render/image/MT_BG_co/MT_BG_co_PuzzleMatte1/PuzzleMatte1.001.exr')
-    u'PuzzleMatte1'
-    """
-
-    if not filename:
-        return
-    if layers is None:
-        with Path(Path(__file__) / '../precomp.redshift.json').open(encoding='UTF-8') as f:
-            layers = json.load(f).get('layers')
-
-    for layer in layers:
-        match = re.search(r'\b({}\d*)\b'.format(layer),
-                          PurePath(filename).name)
-        if match:
-            return unicode(match.group(1))
-
-
-def get_tag(filename, pat=None, default=DEFAULT_TAG):
-    """Return tag of @filename from @pat.
-
-    >>> get_tag('Z:/MT/Render/image/MT_BG_co/MT_BG_co_Z/Z.001.exr', r'MT_(.+)_')
-    u'BG'
-    >>> get_tag('MT_BG_co_Z', r'MT_(.+)_')
-    u'BG'
-    >>> get_tag('Z.001.exr', r'MT_(.+)_')
-    u'Z'
-    >>> get_tag(r'Z:\\QQFC2017\\Render\\SC_065\\QQFC_sc065_CH2')
-    u'CH2'
-    >>> get_tag(r'Z:\\EP13_09_sc151_CH_B\\EP13_09_sc151_CH_B.0015.exr')
-    u'CH_B'
-
-    result of below cases has been auto converted by a dictionary.
-
-    BG_CO -> BG:
-    >>> get_tag('Z:/MT/Render/image/MT_BG_co/MT_BG_co_Z/Z.001.exr')
-    u'BG'
-
-    CH_B_ID -> ID_CH_B:
-    >>> get_tag('Z:/QQFC2017/Render/SC_031a/sc_031a_CH_B_ID/sc_031a_CH_B_ID.####.exr')
-    u'ID_CH_B'
-
-    CH_B_OC -> OCC_CH_B:
-    >>> get_tag('Z:/EP16_05_sc135b_CH_B_OC/EP16_05_sc135b_CH_B_OC.####.exr')
-    u'OCC_CH_B'
-    """
-
-    pat = pat or TAG_PATTERN
-    ret = None
-    path = PurePath(filename)
-    for testing_pat in (pat, TAG_PATTERN):
-        tag_pat = re.compile(testing_pat, flags=re.I)
-        for test_string in\
-                (path.parent.name, path.name):
-            match = re.match(tag_pat, test_string)
-            if match and match.group(1):
-                ret = match.group(1).strip('_').upper()
-                for tag in REGULAR_TAGS:
-                    if ret.startswith(tag):
-                        break
-                else:
-                    LOGGER.warning('不规范标签: %s: %s', ret, filename)
-                break
-        if ret:
-            break
-    else:
-        ret = default
-
-    if TAG_CONVERT_DICT.has_key(ret):
-        ret = TAG_CONVERT_DICT[ret]
-    else:
-        ret = '_'.join(ret.split('_')[:2])
-        ret = TAG_CONVERT_DICT.get(ret, ret)
-
-    if ret.startswith(tuple(string.digits)):
-        ret = '_{}'.format(ret)
-    return get_unicode(ret)
-
-
-def get_shot(path):
-    """Return related shot.  """
-
-    return split_version(PurePath(path).name)[0]
-
-
 def get_server(path):
     r"""Return only path head for unc path.
 
@@ -252,3 +88,309 @@ def escape_batch(text):
     """
 
     return text.replace(u'^', u'^^').replace(u'"', u'\\"').replace(u'|', u'^|')
+
+
+class PurePath(pathlib.PurePath):
+    """Optimized pathlib.PurePath object for footages.  """
+
+    tag_pattern = None
+    version_pattern = r'(.+)v(\d+)'
+    default_tag = DEFAULT_TAG
+    with pathlib.Path(pathlib.Path(__file__)
+                      / '../precomp.redshift.json').open(encoding='UTF-8') as f:
+        layers = json.load(f).get('layers')
+
+    def __new__(cls, *args):
+        """Construct a PurePath from one or several strings and or existing
+        PurePath objects.  The strings and path objects are combined so as
+        to yield a canonicalized path, which is incorporated into the
+        new PurePath object.
+        """
+
+        if cls is PurePath:
+            cls = PureWindowsPath if os.name == 'nt' else PurePosixPath
+        return cls._from_parts(args)
+
+    @property
+    def layer(self):
+        """The footage layer name.
+
+        >>> PurePath('Z:/MT/Render/image/MT_BG_co/MT_BG_co_PuzzleMatte1/PuzzleMatte1.001.exr').layer
+        u'PuzzleMatte1'
+        """
+
+        layers = self.layers
+        if not self:
+            return
+        if layers is None:
+            with pathlib.Path(pathlib.Path(__file__)
+                              / '../precomp.redshift.json').open(encoding='UTF-8') as f:
+                layers = json.load(f).get('layers')
+
+        for layer in layers:
+            match = re.search(r'\b({}\d*)\b'.format(layer),
+                              self.name)
+            if match:
+                return unicode(match.group(1))
+
+    @property
+    def tag(self):
+        """Return footage tag.
+
+        Use custom tag pattern:
+
+        >>> path = PurePath('Z:/MT/Render/image/MT_BG_co/MT_BG_co_Z/Z.001.exr')
+        >>> path.tag_partten = r'MT_(.+)_'
+        >>> path.tag
+        u'BG'
+        >>> path = PurePath('MT_BG_co_Z')
+        >>> path.tag_partten = r'MT_(.+)_'
+        >>> path.tag
+        u'BG'
+        >>> path = PurePath('Z.001.exr')
+        >>> path.tag_partten = r'MT_(.+)_'
+        >>> path.tag
+        u'Z'
+
+        Use default tag pattern:
+
+        >>> PurePath(r'Z:\\QQFC2017\\Render\\SC_065\\QQFC_sc065_CH2').tag
+        u'CH2'
+        >>> PurePath(r'Z:\\EP13_09_sc151_CH_B\\EP13_09_sc151_CH_B.0015.exr').tag
+        u'CH_B'
+
+        result of below cases has been auto converted
+        by a dictionary defined in `__file__/../files.tag.json`.
+
+        >>> # BG_CO -> BG:
+        >>> PurePath('Z:/MT/Render/image/MT_BG_co/MT_BG_co_Z/Z.001.exr').tag
+        u'BG'
+        >>> # CH_B_ID -> ID_CH_B:
+        >>> PurePath('Z:/QQFC2017/Render/SC_031a/sc_031a_CH_B_ID/sc_031a_CH_B_ID.####.exr').tag
+        u'ID_CH_B'
+        >>> # CH_B_OC -> OCC_CH_B
+        >>> PurePath('Z:/EP16_05_sc135b_CH_B_OC/EP16_05_sc135b_CH_B_OC.####.exr').tag
+        u'OCC_CH_B'
+        """
+
+        pat = self.tag_pattern or TAG_PATTERN
+        ret = None
+        path = self
+        for testing_pat in (pat, TAG_PATTERN):
+            tag_pat = re.compile(testing_pat, flags=re.I)
+            for test_string in\
+                    (path.parent.name, path.name):
+                match = re.match(tag_pat, test_string)
+                if match and match.group(1):
+                    ret = match.group(1).strip('_').upper()
+                    for tag in REGULAR_TAGS:
+                        if ret.startswith(tag):
+                            break
+                    else:
+                        LOGGER.warning('不规范标签: %s: %s', ret, self)
+                    break
+            if ret:
+                break
+        else:
+            ret = self.default_tag
+
+        if TAG_CONVERT_DICT.has_key(ret):
+            ret = TAG_CONVERT_DICT[ret]
+        else:
+            ret = '_'.join(ret.split('_')[:2])
+            ret = TAG_CONVERT_DICT.get(ret, ret)
+
+        if ret.startswith(tuple(string.digits)):
+            ret = '_{}'.format(ret)
+        return get_unicode(ret)
+
+    @property
+    def shot(self):
+        """The related shot for this footage.
+
+        >>> PurePath('sc_001_v20.nk').shot
+        u'sc_001'
+        >>> PurePath('hello world').shot
+        u'hello world'
+        >>> PurePath('sc_001_v-1.nk').shot
+        u'sc_001_v-1'
+        >>> PurePath('sc001V1.jpg').shot
+        u'sc001'
+        >>> PurePath('sc001V1_no_bg.jpg').shot
+        u'sc001'
+        >>> PurePath('suv2005_v2_m.jpg').shot
+        u'suv2005'
+        """
+
+        match = re.match(self.version_pattern, self.name, flags=re.I)
+        if not match:
+            return unicode(self.stem)
+        shot = match.group(1)
+        return shot.strip('_')
+
+    @property
+    def version(self):
+        """The nuke style version number of this footage.
+
+        >>> PurePath('sc_001_v20.nk').version
+        20
+        >>> PurePath('hello world').version
+        >>> PurePath('sc_001_v-1.nk').version
+        >>> PurePath('sc001V1.jpg').version
+        1
+        >>> PurePath('sc001V1_no_bg.jpg').version
+        1
+        >>> PurePath('suv2005_v2_m.jpg').version
+        2
+        """
+
+        match = re.match(self.version_pattern, self.name, flags=re.I)
+        if not match:
+            return None
+        return int(match.group(2))
+
+    @property
+    def footage_name(self):
+        """Return filename without frame number.
+
+        >>> get_footage_name('sc_001_BG.0034.exr')
+        u'sc_001_BG'
+        >>> get_footage_name('sc_001_BG.%04d.exr')
+        u'sc_001_BG'
+        >>> get_footage_name('sc_001_BG.###.exr')
+        u'sc_001_BG'
+        >>> get_footage_name('sc_001._BG.exr')
+        u'sc_001._BG'
+        """
+
+        ret = self.name
+        ret = re.sub(r'\.\d+\b', '', ret)
+        ret = re.sub(r'\.#+(?=\.)', '', ret)
+        ret = re.sub(r'\.%0?\d*d\b', '', ret)
+        ret = PurePath(ret).stem
+        return get_unicode(ret)
+
+    def with_frame(self, frame):
+        '''Return a frame mark expaned version of filename, with given frame.
+
+        >>> unicode(PurePath('test_sequence_###.exr').with_frame(1))
+        u'test_sequence_001.exr'
+        >>> unicode(PurePath('test_sequence_369.exr').with_frame(1))
+        u'test_sequence_369.exr'
+        >>> unicode(PurePath('test_sequence_%03d.exr').with_frame(1234))
+        u'test_sequence_1234.exr'
+        >>> unicode(PurePath('test_sequence_%03d.###.exr').with_frame(1234))
+        u'test_sequence_1234.1234.exr'
+        '''
+
+        def _format_repl(matchobj):
+            return matchobj.group(0) % frame
+
+        def _hash_repl(matchobj):
+            return '%0{}d'.format(len(matchobj.group(0)))
+
+        ret = unicode(self)
+        ret = re.sub(r'(\#+)', _hash_repl, ret)
+        ret = re.sub(r'(%0?\d*d)', _format_repl, ret)
+        return PurePath(ret)
+
+    def as_no_version(self):
+        """Return filename without version number.
+
+        >>> unicode(PurePath('sc_001_v233.jpg').as_no_version())
+        u'sc_001.jpg'
+        """
+
+        return self.with_name('{}{}'.format(self.shot, self.suffix))
+
+
+class PurePosixPath(PurePath):
+    """Port from pathlib.PurePosixPath.  """
+
+    _flavour = getattr(pathlib, '_posix_flavour')
+    __slots__ = ()
+
+
+class PureWindowsPath(PurePath):
+    """Port from pathlib.PureWindowsPath.  """
+
+    _flavour = getattr(pathlib, '_windows_flavour')
+    __slots__ = ()
+
+
+class Path(pathlib.Path, PurePath):
+    """Port from pathlib.Path.  """
+
+    def __new__(cls, *args, **kwargs):
+        if cls is Path:
+            cls = WindowsPath if os.name == 'nt' else PosixPath
+        self = cls._from_parts(args, init=False)
+        if not getattr(self, '_flavour').is_supported:
+            raise NotImplementedError("cannot instantiate %r on your system"
+                                      % (cls.__name__,))
+        getattr(self, '_init')()
+        return self
+
+
+class PosixPath(Path, PurePosixPath):
+    """Port from pathlib.PosixPath.  """
+
+    __slots__ = ()
+
+
+class WindowsPath(Path, PureWindowsPath):
+    """Port from pathlib.WindowsPath.  """
+
+    __slots__ = ()
+
+    def owner(self):
+        raise NotImplementedError("Path.owner() is unsupported on this system")
+
+    def group(self):
+        raise NotImplementedError("Path.group() is unsupported on this system")
+
+
+# Remap functions for backward compatibility.
+
+def _expand_frame(filename, frame):
+    return unicode(PurePath(filename).with_frame(frame))
+
+
+def _split_version(f):
+    path = PurePath(f)
+    return path.shot, path.version
+
+
+def _remove_version(path):
+    return unicode(PurePath(path).as_no_version())
+
+
+def _get_shot(path):
+    return PurePath(path).shot
+
+
+def _get_tag(filename, pat=None, default=DEFAULT_TAG):
+    path = PurePath(filename)
+    path.tag_pattern = pat
+    path.default_tag = default
+
+    return path.tag
+
+
+def _get_layer(filename, layers=None):
+    path = PurePath(filename)
+    path.layers = layers
+    return path.layer
+
+
+def _get_footage_name(path):
+    return PurePath(path).footage_name
+
+
+locals()['expand_frame'] = _expand_frame
+locals()['split_version'] = _split_version
+locals()['remove_version'] = _remove_version
+locals()['get_shot'] = _get_shot
+locals()['get_tag'] = _get_tag
+locals()['get_layer'] = _get_layer
+locals()['get_footage_name'] = _get_footage_name
