2fe06374078f4629b2b100b1be82b88c01cc5d4e
NateScarlet <NateScarlet@Gmail.com>
2017-12-04 14:13:37 +0800

    Add scandir.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,11 @@
 """wlf studio common lib.  """
 
 __version__ = '0.1.0'
+
+# Add scandir
+import os
+if 'scandir' not in os.__all__:
+    import wlf.scandir
+    os.__all__.append('scandir')
+    os.scandir = wlf.scandir.scandir
+    os.walk = wlf.scandir.walk
