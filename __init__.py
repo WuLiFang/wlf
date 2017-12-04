43238b6b6f220b6e074b9ed35fb695c9f5614628
NateScarlet <NateScarlet@Gmail.com>
2017-12-04 17:27:22 +0800

    Add bin folder for third party tools.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,20 @@
 # -*- coding=UTF-8 -*-
 """wlf studio common lib.  """
 
+import os
+import sys
+from wlf.path import PurePath
+
 __version__ = '0.1.0'
 
 # Add scandir
-import os
 if 'scandir' not in os.__all__:
     import wlf.scandir
     os.__all__.append('scandir')
     os.scandir = wlf.scandir.scandir
     os.walk = wlf.scandir.walk
+
+# Add bin folder to path.
+BIN_FOLDER = str(PurePath(__file__).parent / PurePath('bin'))
+if BIN_FOLDER not in os.environ['path']:
+    os.environ['path'] = BIN_FOLDER + os.pathsep + os.environ['path']
