d9ae7a0582b102a5607c18b2a3d85fa2c055e2f4
NateScarlet <NateScarlet@Gmail.com>
2018-01-30 16:46:36 +0800

    Add test

--- tests/test_notify.py
+++ tests/test_notify.py
@@ -2,12 +2,16 @@
 """Notify module test.  """
 
 from __future__ import absolute_import
-from unittest import TestCase, main
+
 import time
-import sys
+from threading import Thread
+from unittest import TestCase, main
 
 
 class ProgressTestCase(TestCase):
+    @classmethod
+    def async_test(cls, func):
+        """Run test async.  """
 
     def test_base_handler(self):
         from wlf.notify import progress, BaseProgressHandler
@@ -26,6 +30,17 @@ class ProgressTestCase(TestCase):
 
         for _ in progress(xrange(200), 'qt测试', handler=QtProgressHandler()):
             time.sleep(0.01)
+
+    def test_async(self):
+        threads = []
+        threads.append(Thread(target=self.test_base_handler))
+        threads.append(Thread(target=self.test_cli_handler))
+        threads.append(Thread(target=self.test_qt_handler))
+
+        for i in threads:
+            i.start()
+        for i in threads:
+            i.join()
 
 
 if __name__ == '__main__':
