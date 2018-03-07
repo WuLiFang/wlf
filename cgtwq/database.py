1e216e9e553ed6142a353785fd78eccd6b024637
NateScarlet <NateScarlet@Gmail.com>
2018-03-07 10:13:36 +0800

    cgtwq # Documenting

--- cgtwq/database.py
+++ cgtwq/database.py
@@ -340,18 +340,27 @@ class Selection(list):
         return resp
 
     def link(self, *id_list):
-        resp = self.call(
+        """Link the selection to other items. """
+
+        self.call(
             "c_link", "set_link_id",
             id_array=self, link_id_array=id_list)
-        return resp
 
     def unlink(self, *id_list):
+        """Unlink the selection with other items.  """
+
         for id_ in self:
             self.call(
                 "c_link", "remove_link_id",
                 id=id_, link_id_array=id_list)
 
     def get_linked(self):
+        """Get linked items for the selections.
+
+        Returns:
+            set: All linked item id.
+        """
+
         ret = set()
         for id_ in self:
             resp = self.call("c_link", "get_link_id", id=id_)
