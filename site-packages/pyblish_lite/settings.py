7abe01fee70bfcda909fc23403cdddfcf750d756
NateScarlet <NateScarlet@Gmail.com>
2018-03-07 18:53:23 +0800

    Add `pyblish` to site-packages

---
+++ site-packages/pyblish_lite/settings.py
@@ -0,0 +1,9 @@
+WindowTitle = "Pyblish"  # Customize the window of the pyblish-lite window.
+UseLabel = True  # Customize whether to show label names for plugins.
+
+# Customize which tab to start on. Possible choices are: "artist", "overview"
+# and "terminal".
+InitialTab = "artist"
+
+# Customize the window size.
+WindowSize = (430, 600)
