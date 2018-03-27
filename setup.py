bcd210a4843157e33dcf782be74099ce2ca18966
NateScarlet <NateScarlet@Gmail.com>
2018-03-27 10:28:25 +0800

    Add `install_requires` to `setup.py`

--- setup.py
+++ setup.py
@@ -8,4 +8,13 @@ setup(
     author=__about__.__author__,
     packages=find_packages(),
     package_data={'': ['*.json', '*.png', '*.ui']},
+    install_requires=[
+        'pathlib2==2.3.0',
+        'gevent==1.2.2',
+        'openpyxl==2.5.1',
+        'psutil==5.4.3',
+        'beautifulsoup4==4.6.0',
+        'qt.py==1.1.0',
+        'pyblish==1.4.3',
+    ]
 )
