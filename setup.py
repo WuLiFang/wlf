9536db3a989722ed1e9bbe727877a81bc2b6db00
NateScarlet <NateScarlet@Gmail.com>
2018-03-28 11:25:22 +0800

    Change `setup.py` to accept higher version.

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ setup(
     packages=find_packages(),
     package_data={'': ['*.json', '*.png', '*.ui']},
     install_requires=[
-        'pathlib2==2.3.0',
-        'gevent==1.2.2',
-        'openpyxl==2.5.1',
-        'psutil==5.4.3',
-        'beautifulsoup4==4.6.0',
-        'qt.py==1.1.0',
-        'pyblish==1.4.3',
+        'pathlib2>=2.3.0',
+        'gevent>=1.2.2',
+        'openpyxl>=2.5.1',
+        'psutil>=5.4.3',
+        'beautifulsoup4>=4.6.0',
+        'qt.py>=1.1.0',
+        'pyblish>=1.4.3',
     ]
 )
