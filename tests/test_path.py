1f7d0cbd85829346c7f7f09a9cd76f85374082f6
NateScarlet <NateScarlet@Gmail.com>
2018-05-16 19:58:14 +0800

    Fix test_path

--- tests/test_path.py
+++ tests/test_path.py
@@ -3,6 +3,9 @@
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+
+import sys
+
 from wlf import path
 
 
@@ -22,9 +25,10 @@ def test_get_unicode():
 
 
 def test_get_encoded():
+    encoding = sys.getfilesystemencoding()
     test_case = {
         'a': b'a',
-        '测试'.encode('gbk'): '测试'.encode('utf-8'),
-        '测试2': '测试2'.encode('utf-8')
+        '测试'.encode('gbk'): '测试'.encode(encoding),
+        '测试2': '测试2'.encode(encoding)
     }
     _test_func(path.get_encoded, test_case)
