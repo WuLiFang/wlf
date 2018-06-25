9924c19b99abcefd39faabe57499f468a97b7f83
NateScarlet <NateScarlet@Gmail.com>
2018-06-25 15:32:00 +0800

    Config setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 """Python setup script.  """
 import os
-from setuptools import setup, find_packages
+import sys
+
+from setuptools import find_packages, setup
 
 __about__ = {}
 with open(os.path.join(os.path.dirname(__file__),
                        'wlf', '__about__.py')) as f:
     exec(f.read(), __about__)  # pylint: disable=exec-used
+
+REQUIRES = [
+    'openpyxl>=2.5.1',
+    'psutil>=5.4.3',
+    'beautifulsoup4>=4.6.0',
+    'qt.py>=1.1.0',
+]
+if sys.version.startswith('2.'):
+    REQUIRES.append('pathlib2>=2.3.0')
 
 setup(
     name='wlf',
@@ -13,13 +24,5 @@ setup(
     author=__about__['__author__'],
     packages=find_packages(),
     package_data={'': ['*.json', '*.png', '*.ui']},
-    install_requires=[
-        'pathlib2>=2.3.0',
-        'gevent>=1.2.2',
-        'openpyxl>=2.5.1',
-        'psutil>=5.4.3',
-        'beautifulsoup4>=4.6.0',
-        'qt.py>=1.1.0',
-        'pyblish>=1.4.3',
-    ]
+    install_requires=REQUIRES
 )
