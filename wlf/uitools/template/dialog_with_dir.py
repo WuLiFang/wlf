dd65b7b3f30fb7439654a8e1b6f1ed06a51a8daa
NateScarlet <NateScarlet@Gmail.com>
2023-06-29 14:03:09 +0800

    fix: error in python3

--- wlf/uitools/template/dialog_with_dir.py
+++ wlf/uitools/template/dialog_with_dir.py
@@ -1,7 +1,6 @@
 # -*- coding=UTF-8 -*-
 """UI template.  """
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from abc import abstractmethod
 
@@ -9,10 +8,11 @@ from Qt import QtCompat, QtCore, QtWidgets
 from Qt.QtWidgets import QDialog, QFileDialog
 
 from ...path import Path
+from ... import codectools
 
 
 class DialogWithDir(QDialog):
-    """Dialog with a lineEdit dir input, can restore from config.  """
+    """Dialog with a lineEdit dir input, can restore from config."""
 
     uifile = None
     icons = None
@@ -20,7 +20,10 @@ class DialogWithDir(QDialog):
     def __init__(self, config=None, parent=None):
         self.config = config or dict()
         super(DialogWithDir, self).__init__(parent)
-        QtCompat.loadUi(unicode(Path(__file__, self.uifile)), self)
+        QtCompat.loadUi(
+            codectools.get_unicode(Path(codectools.get_unicode(__file__), self.uifile)),
+            self,
+        )
         self._setup_icons()
         self._load_edits_state()
         self._setup_edits()
@@ -33,7 +36,7 @@ class DialogWithDir(QDialog):
     @property
     @abstractmethod
     def dir_edit(self):
-        """Line edit for dir input.  """
+        """Line edit for dir input."""
 
         return self.lineEditDir
 
@@ -66,25 +69,17 @@ class DialogWithDir(QDialog):
                     lambda e=edit, k=key: _set_config(k, e.text())
                 )
             elif isinstance(edit, QtWidgets.QCheckBox):
-                edit.stateChanged.connect(
-                    lambda state, k=key: _set_config(k, state)
-                )
+                edit.stateChanged.connect(lambda state, k=key: _set_config(k, state))
             elif isinstance(edit, QtWidgets.QComboBox):
                 edit.currentIndexChanged.connect(
-                    lambda index, ex=edit, k=key: _set_config(
-                        k,
-                        ex.itemText(index)
-                    )
+                    lambda index, ex=edit, k=key: _set_config(k, ex.itemText(index))
                 )
             elif isinstance(edit, (QtWidgets.QToolBox, QtWidgets.QTabWidget)):
                 edit.currentChanged.connect(
-                    lambda index, ex=edit, k=key: _set_config(
-                        k,
-                        index
-                    )
+                    lambda index, ex=edit, k=key: _set_config(k, index)
                 )
             else:
-                print(u'待处理的控件: {} {}'.format(type(edit), edit))
+                print("待处理的控件: {} {}".format(type(edit), edit))
 
     def _load_edits_state(self):
         config = self.config
@@ -95,20 +90,17 @@ class DialogWithDir(QDialog):
                 if isinstance(edit, QtWidgets.QLineEdit):
                     edit.setText(config[k])
                 elif isinstance(edit, QtWidgets.QCheckBox):
-                    edit.setCheckState(
-                        QtCore.Qt.CheckState(config[k])
-                    )
+                    edit.setCheckState(QtCore.Qt.CheckState(config[k]))
                 elif isinstance(edit, QtWidgets.QComboBox):
-                    edit.setCurrentIndex(
-                        edit.findText(config[k]))
+                    edit.setCurrentIndex(edit.findText(config[k]))
                 elif isinstance(edit, (QtWidgets.QToolBox, QtWidgets.QTabWidget)):
                     edit.setCurrentIndex(config[k])
             except KeyError as ex:
-                print('wlf.uploader: not found key {} in config'.format(ex))
+                print("wlf.uploader: not found key {} in config".format(ex))
 
     @property
     def directory(self):
-        """Current working dir.  """
+        """Current working dir."""
 
         return self.dir_edit.text()
 
@@ -119,30 +111,30 @@ class DialogWithDir(QDialog):
         if not path.exists():
             return
         path.resolve()
-        value = unicode(path)
+        value = codectools.get_unicode(path)
         if value != self.directory:
             edit.setText(value)
             edit.editingFinished.emit()
 
     def ask_dir(self):
-        """Show a dialog ask user CONFIG['DIR'].  """
+        """Show a dialog ask user CONFIG['DIR']."""
 
         file_dialog = QFileDialog()
         dir_ = file_dialog.getExistingDirectory(
-            dir=unicode(Path(self.directory).parent)
+            dir=codectools.get_unicode(Path(self.directory).parent)
         )
         if dir_:
             self.directory = dir_
 
     def _validate_dir(self):
-        """Check if dir exists.  """
+        """Check if dir exists."""
 
         edit = self.dir_edit
         path = Path(edit.text())
         existed = path.exists()
         if existed:
-            edit.setStyleSheet('')
+            edit.setStyleSheet("")
         else:
-            edit.setStyleSheet('background:rgb(100%,50%,50%)')
+            edit.setStyleSheet("background:rgb(100%,50%,50%)")
 
         return existed
